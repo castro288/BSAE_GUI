e31074953e0343bdc17d9413dde1a744af186dbc
alexandra.cryns <alexandra.cryns@mspag.com>
2017-02-27 15:42:06 +0100

    import jar and make new class

--- src/AE07_GUI/GUI.java
+++
@@ -1,7 +0,0 @@
-package AE07_GUI;
-
-/**
- * Created by alexandra.cryns on 24.02.17.
- */
-public class GUI {
-}
---
+++ src/AE07_GUI/Main.java
@@ -0,0 +1,7 @@
+package AE07_GUI;
+
+/**
+ * Created by alexandra.cryns on 27.02.17.
+ */
+public class Main {
+}
