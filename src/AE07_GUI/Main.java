2e25fe6a4d09be38c9e7fb06e318d0841f96d5b2
alexandra.cryns <alexandra.cryns@mspag.com>
2017-02-28 11:51:46 +0100

    generate main class

--- src/AE07_GUI/Main.java
+++ src/AE07_GUI/Main.java
@@ -4,4 +4,7 @@ package AE07_GUI;
  * Created by alexandra.cryns on 27.02.17.
  */
 public class Main {
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
+    }
 }
--- src/AE07_GUI/MainWindow.java
+++ src/AE07_GUI/MainWindow.java
@@ -1,7 +1,29 @@
 package AE07_GUI;
 
+import javax.swing.*;
+
 /**
  * Created by alexandra.cryns on 27.02.17.
  */
-public class MainWindow {
+public class MainWindow extends JFrame {
+
+    private JTabbedPane tabbedPane1;
+    private JPanel mainPanel;
+    private JPanel tab2;
+    private JPanel tab1;
+    private JPanel tab3;
+    private JPanel tab4;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MainWindow");
+        frame.setContentPane(new MainWindow().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public MainWindow() {
+
+    }
+
 }
